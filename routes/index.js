b8708f8af6e83cca66a603d0231064c2a1512292
Yohsuke Nakano <yohsuke.nak@gmail.com>
2013-09-23 16:07:26 +0900

    first commit

---
+++ routes/index.js
@@ -0,0 +1,17 @@
+exports.index = function(req, res){
+
+	//ユーザーエージェント取得
+	var ua = req.headers['user-agent'].toLowerCase();
+
+	//テンプレート切り替え
+	if(ua.indexOf("android") != -1 || ua.indexOf("iphone") != -1 || ua.indexOf("ipod") != -1){
+		//スマートフォン用テンプレート
+		res.render('index_m', { title: 'スマートフォンサイト' });
+	}
+	else{
+		//PC用テンプレート
+		res.render('index', { title: 'PCサイト' });
+	}
+
+};
+
